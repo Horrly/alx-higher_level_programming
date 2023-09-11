572110d0b329db318b0992f4dab473e3cdee0c0a
Okunola Toheeb <olatundetoheeb0@gmail.com>
2023-09-11 20:22:01 +0200

    javascript

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -7,7 +7,7 @@ const myObject = {
 };
 console.log(myObject);
 myObject.incr = function () {
-  myObject.value++;
+  this.value++;
 };
 myObject.incr();
 console.log(myObject);
--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-// prints two arguments passed to it, in the following format: “ is ”
-
 if (isNaN(process.argv[2])) {
   console.log('Not a number');
 } else {
