b0d4bcadfaadae0dc02cd1d207fd62ba0c5bdd31
Okunola Toheeb <olatundetoheeb0@gmail.com>
2023-09-12 21:53:33 +0200

    my new project

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,17 +1,21 @@
 #!/usr/bin/node
-const dict = require('./101-data').dict;
+// Import the dictionary of occurrences by user id from 101-data.js
+const { dict } = require('./101-data');
 
-const totalist = Object.entries(dict);
-const vals = Object.values(dict);
-const valsUniq = [...new Set(vals)];
-const newDict = {};
-for (const j in valsUniq) {
-  const list = [];
-  for (const k in totalist) {
-    if (totalist[k][1] === valsUniq[j]) {
-      list.unshift(totalist[k][0]);
+// Create a new dictionary to store user ids by occurrence
+const userIDsByOccurrence = {};
+
+// Iterate through the original dictionary
+for (const userID in dict) {
+    const occurrence = dict[userID];
+
+    // If the occurrence is not in the new dictionary, create a new array
+    if (!userIDsByOccurrence[occurrence]) {
+        userIDsByOccurrence[occurrence] = [];
     }
-  }
-  newDict[valsUniq[j]] = list;
+
+
+   userIDsByOccurrence[occurrence].push(userID);
 }
-console.log(newDict);
+console.log(userIDsByOccurrence);
+
